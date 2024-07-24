9d8e709e706e1b3e4fc067147182e3c488d835f7
Mariam andy <mariamalandejanii@gmail.com>
2024-07-24 23:34:08 +0300

    greeting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+<div class="p-16">
+<header class=" flex justify-between">
+<div>
+	 <h1 class="text-4xl mb-4">صباح الخير 👋 <h1/>
+	<h2 class=" text-2xl text-surface-900/60 ">  اليوم، الخميس ٢٠</h2>
+</div>
+	<div class=" bg-red-500 w-20 h-20"> </div>
+</header>
 </div>
