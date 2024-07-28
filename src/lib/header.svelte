d38455b819adf8ca8aa862c32c37b3ce6470e998
Mariam andy <mariamalandejanii@gmail.com>
2024-07-28 19:10:01 +0300

    header component

---
+++ src/lib/header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import 'dayjs/locale/ar';
+
+	dayjs.locale('ar');
+
+	let period=dayjs().format('a') == 'م' ? 'مساء' : 'صباح'
+</script>
+
+<header class=" flex justify-between">
+    <div>
+         <h1 class="text-4xl mb-4"> {period}  الخير 👋 <h1/>
+        <h2 class=" text-2xl text-surface-900/60 "> {dayjs().format('اليوم، dddd D MMM YYYY')} </h2>
+    </div>
+        <div class=" bg-white w-32 h-16"> </div>
+    </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-	import dayjs from 'dayjs';
-	import 'dayjs/locale/ar';
-
-	dayjs.locale('ar');
-
-	let period=dayjs().format('a') == 'م' ? 'مساء' : 'صباح'
+	import Header from "$lib/header.svelte";
 </script>
 
 <div class="p-16">
-<header class=" flex justify-between">
-<div>
-	 <h1 class="text-4xl mb-4"> {period}  الخير 👋 <h1/>
-	<h2 class=" text-2xl text-surface-900/60 "> {dayjs().format('اليوم، dddd D MMM YYYY')} </h2>
-</div>
-	<div class=" bg-white w-32 h-16"> </div>
-</header>
+	<Header />
 </div>
