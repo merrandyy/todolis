ec1c9749a1aef190279f5bdb2b2a04d8f8e34c5e
Mariam andy <mariamalandejanii@gmail.com>
2024-08-15 01:02:32 +0300

    time editing

--- src/lib/tasksinput.svelte
+++ src/lib/tasksinput.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
     import {tasks} from "$lib/stores/tasks";
+    import { Redirect_1 } from "@sveltejs/kit";
 	import dayjs from "dayjs";
 
     let title="";
-    let datetime= dayjs().format("YYYY-MM-DDThh:mm");
+    let datetime= dayjs().format("YYYY-MM-DDTHH:mm");
 
 function addTask() {
     tasks.update((currentTasks) => {
         currentTasks.push({
             title,
-            assignedDate: dayjs().format(),
+            assignedDate: datetime,
             isDone: false,
         });
     
@@ -22,6 +23,6 @@ function addTask() {
 
 <div class="input-group input-group-divider flex flex-col sm:flex-row justify-between !bg-white">
 	<input bind:value={title} class="flex-1 " type="search" placeholder="عنوان المهمة" />
-    <input bind:value={datetime} class="input w-fit" title="Input (datetime-local)" type="datetime-local" />
+    <input dir="ltr" bind:value={datetime} class="input w-fit " title="Input (datetime-local)" type="datetime-local" />
 	<button on:click={addTask} class="variant-filled-primary"> <span class="mx-auto p-1"> اضافة</span></button>
 </div>
