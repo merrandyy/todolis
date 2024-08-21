2759118d22fa2f122d5a579911a09f444da0de17
Mariam andy <mariamalandejanii@gmail.com>
2024-08-22 01:26:24 +0300

    added title and icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,11 @@
 	import Tasksinput from "$lib/tasksinput.svelte";
 	import Taskslist from "$lib/taskslist.svelte";
 
-    
 </script>
+
+<svelte:head>
+	<title> TO DO </title>
+</svelte:head>
 
 <div class="p-6  md:p-8  pt-8 lg:p-16 flex flex-col gap-8 max-w-screen-lg mx-auto">
 
